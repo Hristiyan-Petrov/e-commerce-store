fbae0a64a37bcfbb54fb0edfefdf2eba4651330f
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2025-10-20 16:52:03 +0300

    Add modern and animated mobile menu

--- client/src/components/layout/navbar/Navbar.jsx
+++ client/src/components/layout/navbar/Navbar.jsx
@@ -1,10 +1,12 @@
-import { AppBar, Box, Button, Chip, IconButton, Link, Menu, MenuItem, Toolbar, Typography } from "@mui/material";
+import { AppBar, Backdrop, Box, Button, Chip, Drawer, IconButton, Link, Menu, MenuItem, Toolbar, Typography } from "@mui/material";
 import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined'; import { Link as RouterLink } from "react-router";
 import AccountMenuItem from "./AccountMenuItem";
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import MenuIcon from '@mui/icons-material/Menu';
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
 import { HIDE_MOBILE, SHOW_MD_UP, SHOW_MOBILE_ONLY, SHOW_UP_MD } from "../../../constants/breakpoints";
+import { useState } from "react";
 
 const navigationLinks = [
     { label: 'Shop', to: '/shop' },
@@ -13,13 +15,18 @@ const navigationLinks = [
 ];
 
 const Navbar = () => {
+    const [mobileMenuOpen, setMobileMenu] = useState(false);
+    const toggleMobileMenu = () => setMobileMenu(prevState => !prevState);
+
     const handleSearch = () => {
         console.log('You clicked search.');
     };
+
+
     return (
         <AppBar sx={{ position: 'static', backgroundColor: 'white' }}>
             <Toolbar sx={{
-                justifyContent: {xs: "space-between", xl: 'space-evenly'},
+                justifyContent: { xs: "space-between", xl: 'space-evenly' },
             }}>
 
                 <Box sx={{
@@ -65,51 +72,153 @@ const Navbar = () => {
                                     }
                                 }}
                             >
-                        {link.label}
-                    </Link>
+                                {link.label}
+                            </Link>
                         ))}
+                    </Box>
                 </Box>
+
+                <Box sx={{
+                    display: "flex",
+                    gap: 1,
+                    alignItems: 'center'
+                }}>
+                    <Chip
+                        sx={{
+                            display: HIDE_MOBILE,
+                            pl: 10,
+                            '& .MuiChip-icon': {
+                                color: 'secondary.main'
+                            }
+                        }}
+                        onClick={toggleMobileMenu}
+                        icon={<SearchOutlinedIcon />}
+                    />
+                    <IconButton
+                        sx={{
+                            display: { sm: 'none' },
+                            color: 'secondary.main'
+                        }}
+                        aria-label="search product"
+
+                    >
+                        <SearchOutlinedIcon />
+                    </IconButton>
+
+                    <AccountMenuItem />
+                    <IconButton aria-label="mini shopping cart">
+                        <ShoppingCartOutlinedIcon />
+                    </IconButton>
+
+                    <MobileMenuIcon open={mobileMenuOpen} toggleMobileMenu={toggleMobileMenu}/>
+
+                </Box>
+            </Toolbar>
+            <Box
+                sx={{
+                    position: 'absolute',
+                    top: '56px',
+                    left: 0,
+                    right: 0,
+                    height: 'calc(100vh - 56px)', // 56px is the default Toolbar height on mobile - only place where the mobile menu is visible
+                    overflow: 'hidden',
+                    pointerEvents: open ? 'auto' : 'none',
+                }}
+            >
+                <MobileMenu open={mobileMenuOpen} toggleMobileMenu={toggleMobileMenu} />
             </Box>
+        </AppBar >
+    );
+};
+
+const MobileMenuIcon = ({open, toggleMobileMenu}) => {
+    return (
+        <IconButton
+            aria-label="mobile menu"
+            sx={{
+                display: SHOW_MOBILE_ONLY
+            }}
+            onClick={toggleMobileMenu}
+        >
+
+            {/* {open
+                            ? <CloseRoundedIcon />
+                            : <MenuIcon />
+                        } */}
 
             <Box sx={{
-                display: "flex",
-                gap: 1,
-                alignItems: 'center'
+                position: 'relative',
+                // Set a fixed size to prevent layout shift
+                width: 24,
+                height: 24,
             }}>
-                <Chip
-                    sx={{
-                        display: HIDE_MOBILE,
-                        pl: 10,
-                        '& .MuiChip-icon': {
-                            color: 'secondary.main'
-                        }
-                    }}
-                    onClick={handleSearch}
-                    icon={<SearchOutlinedIcon />}
-                />
-                <IconButton
-                    sx={{
-                        display: { sm: 'none' },
-                        color: 'secondary.main'
-                    }}
-                    aria-label="search product"
-
-                >
-                    <SearchOutlinedIcon />
-                </IconButton>
-
-                <AccountMenuItem />
-                <IconButton aria-label="mini shopping cart">
-                    <ShoppingCartOutlinedIcon />
-                </IconButton>
-                <IconButton aria-label="mobile menu" sx={{
-                    display: SHOW_MOBILE_ONLY
-                }}>
-                    <MenuIcon />
-                </IconButton>
+                <MenuIcon sx={{
+                    position: 'absolute',
+                    transition: 'transform 0.3s, opacity 0.2s',
+                    // When menu is open, rotate and fade out
+                    transform: open ? 'rotate(90deg)' : 'rotate(0deg)',
+                    opacity: open ? 0 : 1,
+                }} />
+                <CloseRoundedIcon sx={{
+                    // position: 'absolute',
+                    transition: 'transform 0.3s, opacity 0.2s',
+                    // When menu is open, rotate in and fade in
+                    transform: open ? 'rotate(0deg)' : 'rotate(-90deg)',
+                    opacity: open ? 1 : 0,
+                }} />
             </Box>
-        </Toolbar>
-        </AppBar >
+
+        </IconButton>
+    );
+};
+
+
+const MobileMenu = ({ open, toggleMobileMenu }) => {
+    return (
+        <Drawer
+            open={open}
+            anchor="top"
+            variant="persistent"
+            ModalProps={{
+                // disablePortal: true,
+            }}
+            PaperProps={{
+                sx: {
+                    position: 'relative',
+                }
+            }}
+        >
+
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    borderTopColor: 'primary.main',
+                    borderTopWidth: '5px',
+                    borderTopStyle: 'solid',
+                }}>
+                {
+                    navigationLinks.map(link => (
+                        <Link
+                            key={link.label}
+                            to={link.to}
+                            component={RouterLink}
+                            onClick={toggleMobileMenu}
+                            sx={{
+                                textDecoration: 'none',
+                                fontWeight: '500',
+                                p: 2,
+                                borderBottomWidth: '2px',
+                                borderBottomStyle: 'solid',
+                                borderBottomColor: 'secondary.light'
+                            }}
+                        >
+                            {link.label}
+                        </Link>
+                    ))
+                }
+            </Box>
+        </Drawer>
     );
 };
 

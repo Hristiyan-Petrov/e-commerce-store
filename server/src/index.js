76f32dad6fbf437a29309af5bcd26d0793a66a84
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2025-10-27 09:23:45 +0200

    Add helmet middleware

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
 const { AppDataSource } = require('./data-source');
 const productRoutes = require('./routes/product');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(helmet());
 
 app.get('/', (req, res) => {
     res.status(200).json({message: 'Hello World!'});

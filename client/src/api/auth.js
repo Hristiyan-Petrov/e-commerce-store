6900cb655756edce70714ba80ff1cf9cd0728632
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2025-11-04 14:36:52 +0200

    Fix auth bugs for guest user

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,6 +1,8 @@
 import { api } from "./fetcher";
 
 export default {
+    getCurrentUser: () => api.get('/auth/me'),
+
     logout: () => {
         return api.get('/auth/logout')
             .then(() => console.log('SUCCESS FROM AUTH API'))
